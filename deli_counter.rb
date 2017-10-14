6415b373b36fa6571d603ca33889c6aaf417f854
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-14 23:49:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
   else
    line = "The line is currently:"
    katz_deli.each.with_index do |index, name|
-   line << "#{index + 1}. #{name}" #shovel add to end of string with beginning space
+   line << " #{index + 1}. #{name}" #shovel add to end of string with beginning space
   end
   puts line
   end
