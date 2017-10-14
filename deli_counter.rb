73803411d820ee8a9922695cbb7e548526dd0c76
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-14 23:46:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,9 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
+   line = "The line is currently:"
    katz_deli.each.with_index(1) do |place_in_line, name|
-   line << "The line is currently: #{place_in_line}. #{name}" #shovel add to end of string with beginning space
+   line <<  #{place_in_line}. #{name}" #shovel add to end of string with beginning space
   end
   end
   puts line
