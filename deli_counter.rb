f2f66ffee62bdd1993055d4addc690b23db5a4d9
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-14 23:52:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
    line = "The line is currently:"
-   katz_deli.each.with_index do |index, name|
+   katz_deli.each.with_index do |name, index|
    line << " #{index + 1}. #{name}" #shovel add to end of string with beginning space
   end
   puts line
