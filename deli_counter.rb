8a28c18c6496363cd0959c5cb7c0fda66706d406
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-15 00:08:30 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -27,6 +27,8 @@ def now_serving(katz_deli)
   if katz_deli.length > 0
     puts "Now serving #{katz_deli.first}."
     katz_deli.shift
-    katz_deli    
+    katz_deli
+  else 
+    puts "There is nobody waiting to be served!""    
   end
 end
